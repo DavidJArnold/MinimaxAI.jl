4a299ed036893ef2acb44ada27f13d1d8ed12c6c
David Arnold <david.14587@gmail.com>
2020-07-06 16:34:11 +1000

    nothing changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Test
 
 
 @testset "Seeing if it works" begin
-    @test playBones(N = 1, ai =2) == nothing
+    @test playBones(N = 2, ai =2) == nothing
     @test playBones(N = 25, ai = 2) == nothing
     @test playTicTacToe(ai = 2) == nothing
     @test playConnect4(ai = 2, method = "minimaxAB", abDepth = 3) == nothing
